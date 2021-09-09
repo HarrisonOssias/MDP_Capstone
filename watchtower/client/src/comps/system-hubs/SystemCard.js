92737843b451701e8e94c0ca420cdad8de3dc9b5
HarrisonOssias <ossiash@mcmaster.ca>
2021-09-09 13:16:12 -0400

    Project Startup

---
+++ watchtower/client/src/comps/system-hubs/SystemCard.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Card, Row, Col } from 'antd';
+import HubMenu from './HubMenu';
+import mockData from './mock.json';
+
+const data = mockData;
+const hubLength = data.length;
+const hubDrops = data.map((hub, index) => {
+	return (
+		<>
+			<Row>
+				<HubMenu hub={hub} />
+			</Row>
+			{hubLength === index + 1 ? null : <div style={{ marginTop: '16px' }} />}
+		</>
+	);
+});
+
+function SystemCard() {
+	return (
+		<Card style={{ height: '93vh', maxHeight: '93vh', overflow: 'auto' }}>
+			<Row>
+				<Col span={18}>{hubDrops}</Col>
+			</Row>
+		</Card>
+	);
+}
+
+export default SystemCard;
