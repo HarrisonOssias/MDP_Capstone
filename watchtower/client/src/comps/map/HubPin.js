a8d0e55edb3fe506c0d2b592e5ddd54e82a96349
HarrisonOssias <ossiash@mcmaster.ca>
2021-09-10 02:47:16 -0400

    ui updates

--- watchtower/client/src/comps/map/HubPin.js
+++ watchtower/client/src/comps/map/HubPin.js
@@ -1,8 +1,16 @@
+import React, { useState, useEffect } from 'react';
 import { CloudUploadOutlined } from '@ant-design/icons';
-const HubPin = () => (
-    <div className="pin">
-      <CloudUploadOutlined style={{ fontSize: '32px', color: 'red' }}/>
-    </div>
-  )
+
+const HubPin = (props) => {
+	const [styles, setStyles] = useState({ fontSize: '32px', color: '#89D3E7' });
+	useEffect(() => {
+		if (props.status == 'Down') setStyles({ fontSize: '32px', color: '#EE6864' });
+	}, []);
+	return (
+		<div className='pin'>
+			<CloudUploadOutlined style={styles} />
+		</div>
+	);
+};
 
 export default HubPin;
--- watchtower/client/src/pages/UserConsole.js
+++ watchtower/client/src/pages/UserConsole.js
@@ -7,51 +7,44 @@ import SystemCard from '../comps/system-hubs/SystemCard';
 import Map from '../comps/map/Map';
 import GraphTabs from '../comps/data-vis/DrawerContent';
 
-
-export const UserContext = createContext()
+export const UserContext = createContext();
 
 export function UserConsole() {
 	const [openDrawer, setOpenDrawer] = useState(false);
 	const [hubList, setHubList] = useState([]);
+	const [currentHub, setCurrentHub] = useState([]);
 
 	const handleDrawer = () => {
+		console.log(hubList);
 		setOpenDrawer(!openDrawer);
 	};
 	const handleClose = () => {
 		setOpenDrawer(false);
 	};
 
-	
 	return (
-		<UserContext.Provider value={{hubList, setHubList, openDrawer, setOpenDrawer}}>
+		<UserContext.Provider value={{ hubList, setHubList, openDrawer, setOpenDrawer, currentHub, setCurrentHub }}>
 			<Row style={{ padding: '6px 0px', display: 'flex', alignItems: 'center', backgroundColor: '#364156', height: '7vh' }}>
 				<NavDrawer />
 				<div>
 					<img src={logo} style={{ height: '50px' }} />
 				</div>
-				<Button type="primary" onClick={() => handleDrawer()}>yessir</Button>
+				<Button type='primary' onClick={() => handleDrawer()}>
+					yessir
+				</Button>
 			</Row>
-			<Row style={{ height: '93vh' }}>
-				<Col span={6} > 
+			<Row style={{ height: '93vh', backgroundColor: '#EDF2F5' }}>
+				<Col span={6} style={{ backgroundColor: '#EDF2F5' }}>
 					<SystemCard />
 				</Col>
-				<Col span={18} style={{ backgroundColor: 'white', overflow: 'hidden' }}>
-			
+				<Col span={18} style={{ backgroundColor: '#EDF2F5', overflow: 'hidden', padding: '20px' }}>
 					<Map />
 					{/*<Image src={mapImg} width={'100%'} height={'100%'} />*/}
-					<Drawer  mask={false} placement='bottom' getContainer={false} style={{ position: 'absolute', margin: "50px 5px 5px 5px" }} onClose={() => handleClose()} visible={openDrawer} height={400} >
+					<Drawer mask={false} placement='bottom' getContainer={false} style={{ position: 'absolute', margin: '50px 5px 5px 5px' }} onClose={() => handleClose()} visible={openDrawer} height={400}>
 						<GraphTabs />
 					</Drawer>
-			
-			
 				</Col>
-			
-
 			</Row>
-			
-			
 		</UserContext.Provider>
 	);
 }
-
-
