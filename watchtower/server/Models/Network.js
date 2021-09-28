c2b5297396aa3f61b184a8c7a17cd9b3f73430e7
HarrisonOssias <ossiash@mcmaster.ca>
2021-09-28 16:10:46 -0400

    serverdev work thursday

---
+++ watchtower/server/Models/Network.js
@@ -0,0 +1,10 @@
+const dynamoose = require('dynamoose');
+
+const Network = new dynamoose.Schema({
+	id: Number,
+	name: String,
+	hub_id: Number,
+	node_ids: [Number],
+});
+
+module.exports = dynamoose.model('Network', Network);
