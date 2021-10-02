d0dc316cd13a9dd6c982a91cdb87274223dc43cc
HarrisonOssias <ossiash@mcmaster.ca>
2021-10-01 22:42:27 -0400

    deleted server

--- watchtower/server/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const dynamoose = require('dynamoose');
-const env = require('dotenv').config();
-
-dynamoose.aws.sdk.config.update({
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	region: process.env.AWS_REGION,
-});
