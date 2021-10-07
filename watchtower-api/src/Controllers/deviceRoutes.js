13644e32c4b01f53e2cb3e9750e1844d5f71bb5d
Jeff Jing <jeffjing98@gmail.com>
2021-10-07 19:59:33 -0400

    works now, also added update route for device table

--- watchtower-api/src/Controllers/deviceRoutes.js
+++ watchtower-api/src/Controllers/deviceRoutes.js
@@ -12,11 +12,52 @@ const deviceRoutes = express.Router({
 deviceRoutes.use(bodyParser.json({ strict: false }));
 
 deviceRoutes.get('/get', async (req, res) => {
-	let getDeviceParams = {
-		TableName: "Device",
-		
+	
+	try {
+		res.status(200).send();
+	} catch (err) {
+		res.status(500).send(err);
 	}
 });
+
+deviceRoutes.put('/put', async (req, res) => {
+	//does the user even add devices themselves again
+	try {
+		res.status(200).send();
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
+
+deviceRoutes.post('/update', async (req, res) => {
+	try {
+		/*
+		"id": "6a54a570-407b-48f1-8ee6-6c35ac54f589",
+		"name": "Hub1",
+		"lat": 43.277704,
+		"lng": -79.92179
+		*/
+		let updateDeviceParam = {
+			TableName: 'Device',
+			Key: {
+				'Id': req.body.id,
+			},
+			UpdateExpression: "SET #deviceName = :name, lat = :lat, lng = :lng",
+			ExpressionAttributeNames: {
+				"#deviceName": "name"
+			},
+			ExpressionAttributeValues: {
+				":name": req.body.name,
+				":lat": req.body.lat,
+				":lng": req.body.lng
+			}
+		}
+		await req.dynamo.update(updateDeviceParam).promise();
+		res.status(200).send(req.body)
+	} catch (err) {
+		res.status(500).send(err);
+	}
+})
 
 
 module.exports = deviceRoutes;
--- watchtower-api/src/routes.js
+++ watchtower-api/src/routes.js
@@ -93,28 +93,33 @@ routes.post('/intake_data', async (req, res) => {
 });
 
 router.get('/get_all', async (req, res) => {
+	//should eventually move the get_all logic to frontend and separate those scans
 	try {
-	  let result = [];
-	  let networks = await dynamoScan.dynamoScan(dynamo, "Network");
-	  let devices = await dynamoScan.dynamoScan(dynamo, "Device");
-	  let datalist = await dynamoScan.dynamoScan(dynamo, "Data", "latest = :boolTrue", { ":boolTrue": true });
-	  networks.map((network, i) => {
-		let hub = devices.find(device => device['Id'] === network.hub_id);
-		let hubData = datalist.find(data => data['device_id'] === network.hub_id);
-		hub.data = { ...hubData.data, timestamp: hubData.timestamp };
-		hub.nodes = [];
-		network.node_ids.values.map((node_id, j) => {
-		  let node = devices.find(device => device['Id'] === node_id);
-		  let nodeData = datalist.find(data => data['device_id'] === node_id);
-		  node.data = { ...nodeData.data, timestamp: nodeData.timestamp };
-		  hub.nodes.push(node)
-		})
-		result.push(hub)
-	  })
-	  res.status(200).send(result)
+		let result = [];
+		let networks = await dynamoScan.dynamoScan(dynamo, "Network");
+		let devices = await dynamoScan.dynamoScan(dynamo, "Device");
+		if (networks.length && devices.length) {
+			let datalist = await dynamoScan.dynamoScan(dynamo, "Data", "latest = :boolTrue", { ":boolTrue": true });
+			networks.map((network, i) => {
+				let hub = devices.find(device => device['Id'] === network.hub_id);
+				let hubData = datalist.find(data => data['device_id'] === network.hub_id);
+				hub.data = { ...hubData.data, timestamp: hubData.timestamp };
+				hub.nodes = [];
+				network.node_ids.values.map((node_id, j) => {
+					let node = devices.find(device => device['Id'] === node_id);
+					let nodeData = datalist.find(data => data['device_id'] === node_id);
+					node.data = { ...nodeData.data, timestamp: nodeData.timestamp };
+					hub.nodes.push(node)
+				})
+				result.push(hub)
+			})
+			res.status(200).send(result)
+		} else {
+			res.status(200).send([]);
+		}
 	} catch (err) {
-	  res.status(500).send(err);
+		res.status(500).send(err);
 	}
-  });
+});
 
 module.exports = routes;
